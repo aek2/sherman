593b9f6751fea95a99496fca50905e5854bac886
aek2 <austinstevenek@gmail.com>
2016-12-22 18:29:19 -0600

    formatting

--- sherman/src/sherman.cpp
+++ sherman/src/sherman.cpp
@@ -40,7 +40,7 @@ int main(int argc, char **argv){
 	        	double min1 = 500;
 	        	double min2 = 500;
 
-			//find the average value on the left and the right
+			//find the minimum value on the left and the right
 	        	for (int i = startQ1; i < endQ1; i++){
 				if (msg.ranges[i] < min1) min1 = msg.ranges[i];
 	        	}
